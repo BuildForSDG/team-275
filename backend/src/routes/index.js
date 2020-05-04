24db49200e257aebd2d2a20eb3d448c06d2b471e
wiredmartian <meggatym@yahoo.com>
2020-05-04 14:44:46 +0200

    test if errors and warnings kill the workflows

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.json("Hello from the backe!");
+  res.json("Hello from the backend!")
 });
 
 module.exports = router;
