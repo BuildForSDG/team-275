5010e018c25d0859c6ff18ec27979c30d108aaf8
wiredmartian <meggatym@yahoo.com>
2020-05-04 14:40:04 +0200

    YML workpace update

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.json("Hello from the backend!!!");
+  res.json("Hello from the backend!");
 });
 
 module.exports = router;
