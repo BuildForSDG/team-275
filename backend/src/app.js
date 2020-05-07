254e765926aae15a419ec6bb1e4a1044168f41cd
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-05-07 12:34:56 +0200

    refactor: export the db connection instance

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { json } from "body-parser";
 import { indexRouter } from "./routes";
+import { connectDB } from "./util/database";
 
 const app = express();
-const mongoose = require("mongoose");
-const uri = "mongodb+srv://team_275:04tLXMQ4mmllwy2P@cluster0-dghpw.mongodb.net/test?retryWrites=true&w=majority";
 app.use(express.json());
 
 /** --- middleware ---- */
@@ -12,14 +11,8 @@ app.use(json());
 /** --- middleware ---- */
 app.use("/api", indexRouter);
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then(result => {
-  app.listen("3000", () => {
-    console.log("Server is running at port 3000");
-  });
-  console.log(result); 
-}).catch(err => console.log(err));
-
-
-
-
+connectDB();
+app.listen("3000", () => {
+  console.log("Server is running at port 3000");
+});
 
