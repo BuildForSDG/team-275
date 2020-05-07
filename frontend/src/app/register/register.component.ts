d5b03d528636f6f46839621fe08ee60c2f304835
TebogoMothelo <tebogo.mothelo@umuzi.org>
2020-05-07 22:30:14 +0200

    feat(validation): added form input validation with bootstrap classes

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { PasswordValidator } from '../password.validator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  options = [
+    'Education Provider', 'Institution'
+  ]
+  get email() {
+    return this.registrationForm.get('email');
   }
 
+  get password() {
+    return this.registrationForm.get('password');
+  }
+  get registeras() {
+    return this.registrationForm.get('registeras');
+  }
+
+  constructor(private fb: FormBuilder) {}
+  registrationForm = this.fb.group({
+    registeras: [''],
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(
+          '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'
+        ),
+      ],
+    ],
+    confirmPassword: [''],
+  }, {validator: PasswordValidator});
+
+  ngOnInit(): void {}
 }
+
