0f390af0698e1f3f35f9e522accf543973855a8d
wiredmartian <meggatym@yahoo.com>
2020-05-07 18:02:43 +0200

    error handling

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,20 +1,19 @@
 import express from "express";
-import {AuthController} from "../controllers/auth.controller";
+import {AuthController} from "../controllers/auth.controller.js";
 
 const router = express.Router();
-const auth = new AuthController();
-router.post("/register", async (req, res) => {
+router.post("/register", async (req, res, next) => {
     try {
+        const auth = new AuthController();
         const user = {
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-            avatar: {type: String}
+            password: req.body.password
         };
         const _newUser = await auth.registerUser(user);
-        res.json(_newUser);
+        res.send(_newUser);
     } catch (e) {
-        res.send(e);
+        res.status(400).send({error: e.message});
     }
 });
 export const userRouter = router;
