f4012e645deaa444c6152814bade233d2174080a
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-05-07 10:49:34 +0200

    feat: set up mongodb and mongoose

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
-import {json} from "body-parser";
-import {indexRouter} from "./routes";
+import { json } from "body-parser";
+import { indexRouter } from "./routes";
 
 const app = express();
+const mongoose = require("mongoose");
+const uri = "mongodb+srv://team_275:04tLXMQ4mmllwy2P@cluster0-dghpw.mongodb.net/test?retryWrites=true&w=majority";
 app.use(express.json());
 
 /** --- middleware ---- */
@@ -10,6 +12,14 @@ app.use(json());
 /** --- middleware ---- */
 app.use("/api", indexRouter);
 
-app.listen("3000", () => {
-  console.log("Server is running at port 3000");
-});
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then(result => {
+  app.listen("3000", () => {
+    console.log("Server is running at port 3000");
+  });
+  console.log(result); 
+}).catch(err => console.log(err));
+
+
+
+
+
