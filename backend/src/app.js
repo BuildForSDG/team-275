eb0bcd62ac2e2047e1888942415b405bb10a458e
wiredmartian <meggatym@yahoo.com>
2020-05-07 18:10:17 +0200

    database connection refactor

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import {json} from "body-parser";
 import {indexRouter} from "./routes";
 import {userRouter} from "./routes/user.route";
+import {dbConnection} from "./util/database";
 
 
 const app = express();
@@ -13,6 +14,10 @@ app.use(json());
 app.use("/api", indexRouter);
 app.use("/api", userRouter);
 
+/** database connection */
+dbConnection.on("error", console.error.bind(console, "connection error:"));
+dbConnection.once("open", console.info.bind(console, "connection established:"));
+/** database connection */
 app.listen("3000", () => {
   console.log("Server is running at port 3000");
 });
--- backend/src/util/database.js
+++ backend/src/util/database.js
@@ -1,17 +1,9 @@
 import mongoose from "mongoose";
 
-/* const db = mongoose;
-const uri = "mongodb+srv://team_275:04tLXMQ4mmllwy2P@cluster0-dghpw.mongodb.net/test?retryWrites=true&w=majority";
-export const connectDB = () => {
-    db.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
-        console.log("DB Connected!!");
-    }).catch(err => console.log(err));
-}; */
-
+/** remote connection */
+/* const uri = "mongodb+srv://team_275:04tLXMQ4mmllwy2P@cluster0-dghpw.mongodb.net/test?retryWrites=true&w=majority"; */
+/** localhost */
 const uri = "mongodb://localhost:27017/team257DB";
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", console.info.bind(console, "connection established:"));
-
 export const dbConnection = db;
