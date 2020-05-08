df7de5e19419c0ab3a129b51f70374b12c4a5c72
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-05-08 20:14:30 +0200

    fix: fix the log in logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,14 +1,29 @@
-import {User} from "../models/user";
+import { User } from "../models/user";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export class AuthController {
     async registerUser(userModel) {
-        let user = await User.findOne({email: userModel.email});
+        let user = await User.findOne({ email: userModel.email });
         if (user) {
             throw new Error("Email already exists!");
         }
         user = new User(userModel);
         user.password = await bcrypt.hash(user.password, 10);
         return user.save();
+    }
+    postLogin(res, req) {
+        User.findOne({ email: req.body.email })
+            .then((doc) => {
+                if (doc) {
+                    if (doc.isValid(req.body.email)) {
+                        let token = jwt.sign({ email: doc.email }, "secret", { expiresIn: "3h" });
+                        return res.status(200).json(token);
+                    }
+                } else {
+                    return res.status(501).json({ message: "User does not exist!" });
+                }
+            })
+            .catch(err => { throw new Error(err); });
     }
 }
