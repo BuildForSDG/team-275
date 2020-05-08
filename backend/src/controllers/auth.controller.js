b314c4754a4e597273ebbbd8ef41762ecf07f70d
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-05-08 17:58:31 +0200

    fix: fix the log in logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -11,34 +11,4 @@ export class AuthController {
         user.password = await bcrypt.hash(user.password, 10);
         return user.save();
     }
-    async postLogin(req, res ) {
-        const signInUser = {
-            email: req.bod.email,
-            password: req.body.password
-        };
-       await User.findOne({email: signInUser.email })
-        .then(user => {
-            if (!user) {
-                return res.redirect("/login");
-            }
-            bcrypt.compare(signInUser.password, user.password)
-            .then(doMatch => {
-                if (doMatch) {
-                    req.session.isLoggedIn = true;
-                    req.session.user = user;
-                   return req.session.save(err => {
-                        console.log(err);
-                        res.redirect("/"); //I'm not sure this is applicable for our monolith
-                    });
-                   
-                }
-                return res.redirect("/login");
-            })
-            .catch(err => {
-                console.log(err);
-                return res.redirect("/login");
-            });
-        })
-        .catch(err => {throw new Error(err);});
-}
 }
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,10 @@
 import express from "express";
 import { AuthController } from "../controllers/auth.controller.js";
+import bcrypt from "bcrypt";
+import { check, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+const config = require("config");
+import {User} from "../models/user";
 
 const router = express.Router();
 router.post("/register", async (req, res) => {
@@ -16,8 +21,58 @@ router.post("/register", async (req, res) => {
         res.status(400).send({ error: e.message });
     }
 });
-router.post("/login", async (res, req) => {
-    const auth = new AuthController();
-    return auth.postLogin(res, req);
-});
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
+router.post(
+    "/login",
+    [
+      check("email", "Please include a valid email").isEmail(),
+      check("password", "Password is required").exists()
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      const { email, password } = req.body;
+  
+      try {
+        let user = await User.findOne({ email });
+  
+        if (!user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+  
+        const isMatch = await bcrypt.compare(password, user.password);
+  
+        if (!isMatch) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+  
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+  
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          { expiresIn: 360000 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+      }
+    });
 export const userRouter = router;
