74dfc3c884d69ff62ea16eb5746cc9f9abc02b84
L. Theodore Obonye <ltobonye@gmail.com>
2020-05-22 15:02:21 +0200

    remove userid validator

--- backend/src/middleware/check-auth.js
+++ backend/src/middleware/check-auth.js
@@ -1,14 +1,12 @@
 import jwt from 'jsonwebtoken';
-import checkObjectId from './checkObjectId';
 
 const checkAuth = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, '1913155164FC4B4DA16CCEA62C6C98A6');
-    if (checkObjectId(decodedToken)) {
-      req.userData = { userId: decodedToken.id };
-      next();
-    }
+  
+    req.userData = { userId: decodedToken.id };  
+    next();
   } catch (err) {
     res.status(401).json({ message: err.message });
   }
