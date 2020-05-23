1df9b85a053de4b1c6f4d692b232dbdbfd93aeee
L. Theodore Obonye <ltobonye@gmail.com>
2020-05-23 13:50:21 +0200

    git add /api/institution/add endpoint

--- backend/src/controllers/institution.controller.js
+++ backend/src/controllers/institution.controller.js
@@ -1,1 +1,23 @@
-export default class InstitutionController {}
+import { Institution } from '../models/institution';
+/**
+ * Add a new Instution.
+ *
+ * @public
+ *
+ * @method
+ * @param - education provider
+ * @param - userId
+ *
+ * @return {Object}        - express http response object
+ */
+export default class InstitutionController {
+  async addInstution(institutionModel, userIdModel) {
+    this.institution = await Institution.findOne({ userId: userIdModel });
+    if (this.institution) {
+      throw new Error('Institution already Exists!');
+    }
+    this.institution = new Institution(institutionModel);
+    this.institution.userId = userIdModel;
+    return this.institution.save();
+  }
+}
