db793aaaed7342b6871e592946a56e24d04882ff
L. Theodore Obonye <ltobonye@gmail.com>
2020-05-23 21:07:31 +0200

    fix: fix linting

--- frontend/src/app/core/password.validator.ts
+++ frontend/src/app/core/password.validator.ts
@@ -1,10 +1,10 @@
-import { AbstractControl } from "@angular/forms";
+import { AbstractControl } from '@angular/forms';
 
 export function PasswordValidator(
   control: AbstractControl
 ): { [key: string]: boolean } | null {
-  const password = control.get("password");
-  const confirmPassword = control.get("confirmPassword");
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
   if (password.pristine || confirmPassword.pristine) {
     return null;
   }
