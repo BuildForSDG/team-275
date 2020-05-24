45c83f43f4adc76bc195d0ac6e5fc831e1aacd38
L. Theodore Obonye <ltobonye@gmail.com>
2020-05-24 12:31:27 +0200

    add home for education provider

--- frontend/src/app/education-provider/education-provider.component.ts
+++ frontend/src/app/education-provider/education-provider.component.ts
@@ -1,6 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
-templateUrl: './education-provider.component.html'
+    templateUrl: './education-provider.component.html',
+    styleUrls: ['./education-provider.component.css']
 })
-export class EducationProviderComponent { }
+export class EducationProviderComponent implements OnInit {
+
+  public context: CanvasRenderingContext2D;
+    public lineChartData: ChartDataSets[] = [
+        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      ];
+      public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+      public lineChartOptions: (ChartOptions ) = {
+        responsive: true,
+      };
+      public lineChartColors: Color[] = [
+        {
+          borderColor: 'black',
+          backgroundColor: 'rgba(255,0,0,0.3)',
+        },
+      ];
+      public lineChartLegend = true;
+      public lineChartType = 'line';
+      public lineChartPlugins = [];
+      constructor() { }
+    ngOnInit() { }
+}
--- frontend/src/app/education-provider/education-provider.module.ts
+++ frontend/src/app/education-provider/education-provider.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EducationProviderComponent } from './education-provider.component';
 import { EducationProviderRoutingModule } from './education-provider-routing.module';
+import { ChartsModule } from 'ng2-charts';
 
 
 
@@ -9,7 +10,8 @@ import { EducationProviderRoutingModule } from './education-provider-routing.mod
   declarations: [EducationProviderComponent],
   imports: [
     CommonModule,
-    EducationProviderRoutingModule
+    EducationProviderRoutingModule,
+    ChartsModule
   ],
   exports: [EducationProviderComponent]
 })
